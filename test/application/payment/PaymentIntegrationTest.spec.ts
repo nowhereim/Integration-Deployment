29bb478d0261af95bd79c50b92f523b442f02bc6
anth <zyle1104@gmail.com>
2024-09-21 19:13:46 +0900

    ✅ Test:

--- test/application/payment/PaymentIntegrationTest.spec.ts
+++ test/application/payment/PaymentIntegrationTest.spec.ts
@@ -67,47 +67,41 @@ describe('PaymentFacade Integration Test', () => {
     await container.stop();
   });
   describe('결제 생성', () => {
-    // it('결제 생성 성공', async () => {
-    //   const userId = 1;
-    //   const seatId = 1;
-    //   const concertId = 1;
-    //   // const amount = 10000;
-    //   // await queueFacadeApp.registerQueue({ userId });/
-    //   // await userFacadeApp.cashCharge({
-    //   //   userId,
-    //   //   amount,
-    //   // });
-    //   await reservationFacade.registerReservation({
-    //     userId,
-    //     seatId,
-    //     concertId,
-    //   });
-    //   const payment = await paymentFacade.pay({
-    //     userId,
-    //     seatId,
-    //   });
-    //   expect(payment).toEqual({
-    //     id: expect.any(Number),
-    //     userId,
-    //     status: 'PENDING',
-    //     seatNumber: expect.any(Number),
-    //     openAt: expect.any(Date),
-    //     concertName: expect.any(String),
-    //     closeAt: expect.any(Date),
-    //     totalAmount: expect.any(Number),
-    //   });
-    // }, 60000);
-    // it('이미 결제된 좌석에 대한 결제 시도 실패', async () => {
-    //   const userId = 1;
-    //   const seatId = 1;
+    it('결제 생성 성공', async () => {
+      const userId = 1;
+      const seatId = 1;
+      const concertId = 1;
+      await reservationFacade.registerReservation({
+        userId,
+        seatId,
+        concertId,
+      });
+      const payment = await paymentFacade.pay({
+        userId,
+        seatId,
+      });
+      expect(payment).toEqual({
+        id: expect.any(Number),
+        userId,
+        status: 'PENDING',
+        seatNumber: expect.any(Number),
+        openAt: expect.any(Date),
+        concertName: expect.any(String),
+        closeAt: expect.any(Date),
+        totalAmount: expect.any(Number),
+      });
+    }, 60000);
+    it('이미 결제된 좌석에 대한 결제 시도 실패', async () => {
+      const userId = 1;
+      const seatId = 1;
 
-    //   await expect(
-    //     paymentFacade.pay({
-    //       userId,
-    //       seatId,
-    //     }),
-    //   ).rejects.toThrow(NotFoundException);
-    // });
+      await expect(
+        paymentFacade.pay({
+          userId,
+          seatId,
+        }),
+      ).rejects.toThrow(NotFoundException);
+    });
 
     it('대기열에 등록되지 않은 사용자 결제 시도 실패', async () => {
       const userId = 100;
@@ -121,31 +115,4 @@ describe('PaymentFacade Integration Test', () => {
       ).rejects.toThrow(NotFoundException);
     }, 60000);
   });
-
-  // describe('결제 유저 캐시 동시성 테스트', () => {
-  //   it('동일한 결제건에 대하여 중복 요청시 금액 차감은 하나만 반영되어야한다.', async () => {
-  //     const userId = 1;
-  //     const seatId = 1;
-  //     const concertId = 1;
-
-  //     await reservationFacade.registerReservation({
-  //       userId,
-  //       seatId,
-  //       concertId,
-  //     });
-
-  //     const promises = Array.from({ length: 5 }).map(() =>
-  //       paymentFacade.pay({
-  //         userId,
-  //         seatId,
-  //       }),
-  //     );
-
-  //     await Promise.allSettled(promises);
-  //     const getUserCash = await userFacadeApp.cashRead({
-  //       userId,
-  //     });
-  //     expect(getUserCash.cash.getVersion()).toBe(2); // 버전컬럼이 1 => 2로 증가해야한다.
-  //   }, 60000);
-  // });
 });

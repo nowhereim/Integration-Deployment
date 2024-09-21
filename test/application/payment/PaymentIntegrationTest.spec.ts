e83d344ebe09282e8d6417d62eeaad579a5a3fb0
anth <zyle1104@gmail.com>
2024-09-21 19:51:17 +0900

    ✅ Test:

--- test/application/payment/PaymentIntegrationTest.spec.ts
+++ test/application/payment/PaymentIntegrationTest.spec.ts
@@ -3,19 +3,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 import { PaymentFacadeApp } from 'src/application/payment/payment.facade';
-// import { QueueFacadeApp } from 'src/application/queue/queue.facade';
-// import { ReservationFacadeApp } from 'src/application/reservation/reservation.facade';
-// import { UserFacadeApp } from 'src/application/user/user.facade';
+import { QueueFacadeApp } from 'src/application/queue/queue.facade';
+import { ReservationFacadeApp } from 'src/application/reservation/reservation.facade';
+import { UserFacadeApp } from 'src/application/user/user.facade';
 import { PaymentModule } from 'src/modules/payment.module';
 import { SeederService } from 'src/seed/seeder.service';
-import { GenericContainer, StartedTestContainer, Wait } from 'testcontainers';
+import { GenericContainer, StartedTestContainer } from 'testcontainers';
 
 describe('PaymentFacade Integration Test', () => {
   let app: INestApplication;
   let paymentFacade: PaymentFacadeApp;
-  // let reservationFacade: ReservationFacadeApp;
-  // let userFacadeApp: UserFacadeApp;
-  // let queueFacadeApp: QueueFacadeApp;
+  let reservationFacade: ReservationFacadeApp;
+  let userFacadeApp: UserFacadeApp;
+  let queueFacadeApp: QueueFacadeApp;
   let seederService: SeederService;
   let container: StartedTestContainer;
 
@@ -65,47 +65,47 @@ describe('PaymentFacade Integration Test', () => {
     await container.stop();
   });
   describe('결제 생성', () => {
-    // it('결제 생성 성공', async () => {
-    //   const userId = 1;
-    //   const seatId = 1;
-    //   const concertId = 1;
-    //   // const amount = 10000;
-    //   // await queueFacadeApp.registerQueue({ userId });/
-    //   // await userFacadeApp.cashCharge({
-    //   //   userId,
-    //   //   amount,
-    //   // });
-    //   await reservationFacade.registerReservation({
-    //     userId,
-    //     seatId,
-    //     concertId,
-    //   });
-    //   const payment = await paymentFacade.pay({
-    //     userId,
-    //     seatId,
-    //   });
-    //   expect(payment).toEqual({
-    //     id: expect.any(Number),
-    //     userId,
-    //     status: 'PENDING',
-    //     seatNumber: expect.any(Number),
-    //     openAt: expect.any(Date),
-    //     concertName: expect.any(String),
-    //     closeAt: expect.any(Date),
-    //     totalAmount: expect.any(Number),
-    //   });
-    // }, 60000);
-    // it('이미 결제된 좌석에 대한 결제 시도 실패', async () => {
-    //   const userId = 1;
-    //   const seatId = 1;
+    it('결제 생성 성공', async () => {
+      const userId = 1;
+      const seatId = 1;
+      const concertId = 1;
+      const amount = 10000;
+      await queueFacadeApp.registerQueue({ userId });
+      await userFacadeApp.cashCharge({
+        userId,
+        amount,
+      });
+      await reservationFacade.registerReservation({
+        userId,
+        seatId,
+        concertId,
+      });
+      const payment = await paymentFacade.pay({
+        userId,
+        seatId,
+      });
+      expect(payment).toEqual({
+        id: expect.any(Number),
+        userId,
+        status: 'PENDING',
+        seatNumber: expect.any(Number),
+        openAt: expect.any(Date),
+        concertName: expect.any(String),
+        closeAt: expect.any(Date),
+        totalAmount: expect.any(Number),
+      });
+    }, 60000);
+    it('이미 결제된 좌석에 대한 결제 시도 실패', async () => {
+      const userId = 1;
+      const seatId = 1;
 
-    //   await expect(
-    //     paymentFacade.pay({
-    //       userId,
-    //       seatId,
-    //     }),
-    //   ).rejects.toThrow(NotFoundException);
-    // });
+      await expect(
+        paymentFacade.pay({
+          userId,
+          seatId,
+        }),
+      ).rejects.toThrow(NotFoundException);
+    });
 
     it('대기열에 등록되지 않은 사용자 결제 시도 실패', async () => {
       const userId = 100;

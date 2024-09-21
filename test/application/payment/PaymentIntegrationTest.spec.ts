142b058c96a43f4c73946cb894d98d0d78b63545
anth <zyle1104@gmail.com>
2024-09-21 23:49:40 +0900

    ✅ Test: 결제 통합테스트 테스트케이스 추가
    결제 생성, 결제 상태변경 테스트 케이스 추가

--- test/application/payment/PaymentIntegrationTest.spec.ts
+++ test/application/payment/PaymentIntegrationTest.spec.ts
@@ -2,21 +2,14 @@ import { INestApplication, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
-import { PaymentFacadeApp } from 'src/application/payment/payment.facade';
+import { PaymentStatus } from 'src/domain/payment/payment';
 import { PaymentService } from 'src/domain/payment/payment.service';
-// import { QueueFacadeApp } from 'src/application/queue/queue.facade';
-// import { ReservationFacadeApp } from 'src/application/reservation/reservation.facade';
-// import { UserFacadeApp } from 'src/application/user/user.facade';
 import { PaymentModule } from 'src/modules/payment.module';
 import { SeederService } from 'src/seed/seeder.service';
 import { GenericContainer, StartedTestContainer } from 'testcontainers';
 
 describe('PaymentFacade Integration Test', () => {
   let app: INestApplication;
-  let paymentFacade: PaymentFacadeApp;
-  // let reservationFacade: ReservationFacadeApp;
-  // let userFacadeApp: UserFacadeApp;
-  // let queueFacadeApp: QueueFacadeApp;
   let seederService: SeederService;
   let container: StartedTestContainer;
   let paymentService: PaymentService;
@@ -27,7 +20,7 @@ describe('PaymentFacade Integration Test', () => {
       .withEnvironment({
         MYSQL_ROOT_PASSWORD: 'root',
         MYSQL_DATABASE: 'concert',
-      }) // MYSQL_DATABASE 설정 추가
+      })
       .withExposedPorts(3306)
       .start();
 
@@ -44,7 +37,6 @@ describe('PaymentFacade Integration Test', () => {
             database: 'concert',
             entities: [path.join(__dirname, '../../../**/*.entity.ts')],
             synchronize: true,
-            // logging: true,
           }),
         }),
 
@@ -55,7 +47,6 @@ describe('PaymentFacade Integration Test', () => {
 
     app = module.createNestApplication();
 
-    paymentFacade = module.get<PaymentFacadeApp>(PaymentFacadeApp);
     seederService = module.get<SeederService>(SeederService);
     paymentService = module.get<PaymentService>(PaymentService);
 
@@ -68,28 +59,35 @@ describe('PaymentFacade Integration Test', () => {
     await container.stop();
   });
   describe('결제 생성', () => {
-    // it('결제 생성 성공', async () => {
-    //   const userId = 1;
-    //   const seatId = 1;
-    //   const payment = await paymentFacade.pay({
-    //     userId,
-    //     seatId,
-    //   });
-    //   expect(payment).toEqual({
-    //     id: expect.any(Number),
-    //     userId,
-    //     status: 'PENDING',
-    //     seatNumber: expect.any(Number),
-    //     openAt: expect.any(Date),
-    //     concertName: expect.any(String),
-    //     closeAt: expect.any(Date),
-    //     totalAmount: expect.any(Number),
-    //   });
-    // }, 60000);
-    it('이미 결제된 좌석에 대한 결제 시도 실패', async () => {
+    it('결제 생성 성공', async () => {
       const userId = 1;
       const seatNumber = 1;
-      await paymentService.pay({
+      const payment = await paymentService.pay({
+        userId,
+        seatNumber,
+        concertName: '박효신 콘서트',
+        openAt: new Date(new Date().getTime() - 1000 * 60),
+        closeAt: new Date(new Date().getTime() + 1000 * 60 * 60 * 60 * 60),
+        totalAmount: 10000,
+      });
+      expect(payment).toEqual({
+        id: expect.any(Number),
+        userId,
+        status: 'PENDING',
+        seatNumber: expect.any(Number),
+        openAt: expect.any(Date),
+        concertName: expect.any(String),
+        closeAt: expect.any(Date),
+        totalAmount: expect.any(Number),
+      });
+    }, 60000);
+  });
+
+  describe('결제 상태 변경', () => {
+    it('결제 상태 완료로 변경', async () => {
+      const userId = 1;
+      const seatNumber = 1;
+      const payment = await paymentService.pay({
         userId,
         seatNumber,
         concertName: '박효신 콘서트',
@@ -98,51 +96,64 @@ describe('PaymentFacade Integration Test', () => {
         totalAmount: 10000,
       });
 
-      // await expect(
-      //   paymentFacade.pay({
-      //     userId,
-      //     seatId,
-      //   }),
-      // ).rejects.toThrow(NotFoundException);
-    }, 60000);
+      const completedPayment = await paymentService.completePayment({
+        paymentId: payment.id,
+      });
 
-    it('대기열에 등록되지 않은 사용자 결제 시도 실패', async () => {
-      const userId = 100;
-      const seatId = 5;
+      expect(completedPayment).toEqual({
+        id: expect.any(Number),
+        userId,
+        status: PaymentStatus.COMPLETED,
+        seatNumber: expect.any(Number),
+        openAt: expect.any(Date),
+        concertName: expect.any(String),
+        closeAt: expect.any(Date),
+        totalAmount: expect.any(Number),
+      });
+    });
 
+    it('결제 상태 실패로 변경', async () => {
+      const userId = 1;
+      const seatNumber = 1;
+      const payment = await paymentService.pay({
+        userId,
+        seatNumber,
+        concertName: '박효신 콘서트',
+        openAt: new Date(new Date().getTime() - 1000 * 60),
+        closeAt: new Date(new Date().getTime() + 1000 * 60 * 60 * 60 * 60),
+        totalAmount: 10000,
+      });
+
+      const failedPayment = await paymentService.failPayment({
+        paymentId: payment.id,
+      });
+
+      expect(failedPayment).toEqual({
+        id: expect.any(Number),
+        userId,
+        status: PaymentStatus.FAILED,
+        seatNumber: expect.any(Number),
+        openAt: expect.any(Date),
+        concertName: expect.any(String),
+        closeAt: expect.any(Date),
+        totalAmount: expect.any(Number),
+      });
+    });
+
+    it('존재하지 않는 결제 상태 완료로 변경 실패', async () => {
       await expect(
-        paymentFacade.pay({
-          userId,
-          seatId,
+        paymentService.completePayment({
+          paymentId: 999,
         }),
       ).rejects.toThrow(NotFoundException);
-    }, 60000);
+    });
+
+    it('존재하지 않는 결제 상태 실패로 변경 실패', async () => {
+      await expect(
+        paymentService.failPayment({
+          paymentId: 999,
+        }),
+      ).rejects.toThrow(NotFoundException);
+    });
   });
-
-  // describe('결제 유저 캐시 동시성 테스트', () => {
-  //   it('동일한 결제건에 대하여 중복 요청시 금액 차감은 하나만 반영되어야한다.', async () => {
-  //     const userId = 1;
-  //     const seatId = 1;
-  //     const concertId = 1;
-
-  //     await reservationFacade.registerReservation({
-  //       userId,
-  //       seatId,
-  //       concertId,
-  //     });
-
-  //     const promises = Array.from({ length: 5 }).map(() =>
-  //       paymentFacade.pay({
-  //         userId,
-  //         seatId,
-  //       }),
-  //     );
-
-  //     await Promise.allSettled(promises);
-  //     const getUserCash = await userFacadeApp.cashRead({
-  //       userId,
-  //     });
-  //     expect(getUserCash.cash.getVersion()).toBe(2); // 버전컬럼이 1 => 2로 증가해야한다.
-  //   }, 60000);
-  // });
 });
